9fd6d7507bfc25dd9ef412c6b2f126bd02beef40
seanderoo <sean.roo@koop.overheid.nl>
2026-04-21 13:32:54 +0200

    Initialize project with Spring Boot and Micrometer metrics

---
+++ src/main/java/com/example/custommetrics/CustomMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.example.custommetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomMetricsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/custommetrics/CustomMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.custommetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
